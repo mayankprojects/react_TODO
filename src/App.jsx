174729165aa0b6b7856969c5138017d19fbfed7c
Mayank <kumar113349@gmail.com>
2024-09-02 17:46:47 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,55 +4,12 @@ import Todos from "./components/Todos";
 import Todostart from "./components/Todostart";
 import Error from "./components/Error";
 import { TodoContext } from "./store/todo-items-store";
+import Todoitemwrapper from "./store/todo-items-store";
 
-const Reducer = (currState, action) => {
-  let newState = currState;
-  if (action.type === "ADD") {
-    newState = [
-      ...currState,
-      {
-        name: action.payload.task,
-        dueDate: action.payload.date,
-      },
-    ];
-  } else if (action.type === "DELETE") {
-    newState = currState.filter((ele) => ele.name !== action.payload.task);
-  }
-  return newState;
-};
 function App() {
-  let [items, Dispatch] = useReducer(Reducer, []);
-
-  const handleAdd = (task, date) => {
-    const addAction = {
-      type: "ADD",
-      payload: {
-        task,
-        date,
-      },
-    };
-    Dispatch(addAction);
-  };
-
-  const handleDelete = (task) => {
-    const delAction = {
-      type: "DELETE",
-      payload: {
-        task,
-      },
-    };
-    Dispatch(delAction);
-  };
-
   return (
     <>
-      <TodoContext.Provider
-        value={{
-          items: items,
-          handleAdd: handleAdd,
-          handleDelete: handleDelete,
-        }}
-      >
+      <Todoitemwrapper>
         <div className="container flex h-full justify-center items-center flex-col mt-12">
           <h2 className="font-bold text-3xl text-center mb-4 mr-32">
             Todo App
@@ -63,7 +20,7 @@ function App() {
             <Todoitems></Todoitems>
           </div>
         </div>
-      </TodoContext.Provider>
+      </Todoitemwrapper>
     </>
   );
 }
--- src/store/todo-items-store.jsx
+++ src/store/todo-items-store.jsx
@@ -1,10 +1,63 @@
-import { createContext } from "react";
-
+import { Children, createContext } from "react";
+import { useReducer } from "react";
 export const TodoContext = createContext({
-    items: [],
-    handleAdd: () => {},
-    handleDelete: () => {},
+  items: [],
+  handleAdd: () => {},
+  handleDelete: () => {},
 });
 // export const TodoContext = createContext([{name: "buy milk", dueDate: "today"}]);
 
+const Reducer = (currState, action) => {
+  let newState = currState;
+  if (action.type === "ADD") {
+    newState = [
+      ...currState,
+      {
+        name: action.payload.task,
+        dueDate: action.payload.date,
+      },
+    ];
+  } else if (action.type === "DELETE") {
+    newState = currState.filter((ele) => ele.name !== action.payload.task);
+  }
+  return newState;
+};
 
+export default function Todoitemwrapper({children}) {
+  let [items, Dispatch] = useReducer(Reducer, []);
+
+  const handleAdd = (task, date) => {
+    const addAction = {
+      type: "ADD",
+      payload: {
+        task,
+        date,
+      },
+    };
+    Dispatch(addAction);
+  };
+
+  const handleDelete = (task) => {
+    const delAction = {
+      type: "DELETE",
+      payload: {
+        task,
+      },
+    };
+    Dispatch(delAction);
+  };
+
+  return (
+    <>
+      <TodoContext.Provider
+        value={{
+          items: items,
+          handleAdd: handleAdd,
+          handleDelete: handleDelete,
+        }}
+      >
+        {children}
+      </TodoContext.Provider>
+    </>
+  );
+}
